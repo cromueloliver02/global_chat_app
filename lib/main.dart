6449d61e867657de7efeab3f954789b58034502f
Cromuel Oliver <cromuelbarut@gmail.com>
2024-03-20 00:22:50 +0800

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'package:global_chat/firebase_options.dart';
 import 'package:global_chat/global_chat_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const GlobalChatApp());
 }
