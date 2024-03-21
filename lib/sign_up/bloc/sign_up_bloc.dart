627c1e59e01acea41302039b4aef23f833137380
Cromuel Oliver <cromuelbarut@gmail.com>
2024-03-21 13:01:30 +0800

    implement auth sign-up in the sign up bloc

--- lib/sign_up/bloc/sign_up_bloc.dart
+++ lib/sign_up/bloc/sign_up_bloc.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
+import 'package:auth_repository/auth_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:core_utils/core_utils.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:formz/formz.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:global_chat/core/formz_inputs/formz_inputs.dart';
@@ -15,7 +17,12 @@ part 'sign_up_event.dart';
 part 'sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc() : super(SignUpState.initial()) {
+  final AuthRepository _authRepository;
+
+  SignUpBloc({
+    required AuthRepository authRepository,
+  })  : _authRepository = authRepository,
+        super(SignUpState.initial()) {
     on<SignUpSubmitted>(_onSignUpSubmitted);
     on<SignUpUsernameChanged>(_onSignUpUsernameChanged);
     on<SignUpEmailChanged>(_onSignUpEmailChanged);
@@ -25,7 +32,38 @@ class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
   Future<void> _onSignUpSubmitted(
     SignUpSubmitted event,
     Emitter<SignUpState> emit,
-  ) async {}
+  ) async {
+    emit(state.copyWith(autovalidateMode: AutovalidateMode.always));
+
+    final bool isValid = Formz.validate([
+      state.usernameInput,
+      state.emailInput,
+      state.passwordInput,
+    ]);
+
+    if (!isValid) return;
+
+    emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
+
+    final Either<Failure, void> either = await _authRepository.signUp(
+      username: state.usernameInput.value!,
+      email: state.emailInput.value!,
+      password: state.passwordInput.value!,
+    );
+
+    either.fold(
+      (Failure failure) {
+        debugPrint(failure.toString());
+        emit(
+          state.copyWith(
+            status: FormzSubmissionStatus.failure,
+            failure: failure,
+          ),
+        );
+      },
+      (_) => emit(const SignUpState(status: FormzSubmissionStatus.success)),
+    );
+  }
 
   void _onSignUpUsernameChanged(
     SignUpUsernameChanged event,
