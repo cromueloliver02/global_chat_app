41d6b79aca9c23aac055be75bee0c1a3676c4bc0
Cromuel Oliver <cromuelbarut@gmail.com>
2024-03-21 12:57:42 +0800

    implement auth sign-in in the sign in bloc

--- lib/sign_in/bloc/sign_in_bloc.dart
+++ lib/sign_in/bloc/sign_in_bloc.dart
@@ -1,8 +1,10 @@
+import 'package:auth_repository/auth_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:core_utils/core_utils.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:formz/formz.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:global_chat/core/formz_inputs/formz_inputs.dart';
@@ -12,7 +14,12 @@ part 'sign_in_event.dart';
 part 'sign_in_state.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
-  SignInBloc() : super(SignInState.initial()) {
+  final AuthRepository _authRepository;
+
+  SignInBloc({
+    required AuthRepository authRepository,
+  })  : _authRepository = authRepository,
+        super(SignInState.initial()) {
     on<SignInSubmitted>(_onSignInSubmitted);
     on<SignInEmailChanged>(_onAuthEmailChanged);
     on<SignInPasswordChanged>(_onSignInPasswordChanged);
@@ -21,7 +28,36 @@ class SignInBloc extends Bloc<SignInEvent, SignInState> {
   Future<void> _onSignInSubmitted(
     SignInSubmitted event,
     Emitter<SignInState> emit,
-  ) async {}
+  ) async {
+    emit(state.copyWith(autovalidateMode: AutovalidateMode.always));
+
+    final bool isValid = Formz.validate([
+      state.emailInput,
+      state.passwordInput,
+    ]);
+
+    if (!isValid) return;
+
+    emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
+
+    final Either<Failure, void> either = await _authRepository.signIn(
+      email: state.emailInput.value!,
+      password: state.passwordInput.value!,
+    );
+
+    either.fold(
+      (Failure failure) {
+        debugPrint(failure.toString());
+        emit(
+          state.copyWith(
+            status: FormzSubmissionStatus.failure,
+            failure: failure,
+          ),
+        );
+      },
+      (_) => emit(const SignInState(status: FormzSubmissionStatus.success)),
+    );
+  }
 
   void _onAuthEmailChanged(
     SignInEmailChanged event,
