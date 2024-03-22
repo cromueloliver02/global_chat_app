ae936e58a084523181e484a510ffa024890b9846
Cromuel Oliver <cromuelbarut@gmail.com>
2024-03-22 15:02:54 +0800

    update firebase error status code for invalid login credentials

--- packages/core_utils/lib/src/utils/firebase_error_code.dart
+++ packages/core_utils/lib/src/utils/firebase_error_code.dart
@@ -2,6 +2,6 @@ class FirebaseErrorCode {
   const FirebaseErrorCode._();
 
   static const String networkRequestFailed = 'network-request-failed';
-  static const String invalidLoginCredentials = 'INVALID_LOGIN_CREDENTIALS';
+  static const String invalidLoginCredentials = 'invalid-credential';
   static const String tooManyRequests = 'too-many-requests';
 }
